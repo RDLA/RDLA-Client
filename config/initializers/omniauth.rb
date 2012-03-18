ed4758339575c851add5732762d66dca96327eca
Damien Brian <damien.brian@rdla.fr>
2012-03-18 18:21:37 +0100

    Allow developepr log

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :developer unless Rails.env.production?
+  #provider :developer unless Rails.env.production?
+  provider :developer
    provider :facebook, "285503821517899", "ad01f47e7d4f7efe8f77ae50461e9046",
            :scope => 'email', :display => 'page'
 end
