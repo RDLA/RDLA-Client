5e53e6ca67eef7972f7553a4f11c5f4d4599f5eb
Damien Brian <damien.brian@rdla.fr>
2012-03-26 18:56:34 +0200

    Disable Developer auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  #provider :developer unless Rails.env.production?
-  provider :developer
-   provider :facebook, "285503821517899", "ad01f47e7d4f7efe8f77ae50461e9046",
+  provider :developer unless Rails.env.production?
+  provider :facebook, "285503821517899", "ad01f47e7d4f7efe8f77ae50461e9046",
            :scope => 'email', :display => 'page'
 end
