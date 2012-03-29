6acb1308fe4cb8c100d0949c8e8e554722381f8b
Damien Brian <damien.brian@rdla.fr>
2012-03-29 15:52:33 +0200

    Add env options for securing client, and allow client to compile sprite field sheet

---
+++ config/initializers/external.rb
@@ -0,0 +1,7 @@
+DB_URL = "http://mistral.rdla.fr/api/"
+ASSETS_URL = "http://assets.rdla.fr/"
+FTP_URL = "ftp.rdla.fr"
+FTP_USER = ENV['FTP_USER']
+FTP_PASSWORD = ENV['FTP_PASSWORD']
+FACEBOOK_LOGIN = ENV['FACEBOOK_LOGIN']
+FACEBOOK_PASSWORD = ENV['FACEBOOK_PASSWORD']
