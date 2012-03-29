ddbc44904058d2456ab2e63d533c20c1687579af
Damien Brian <damien.brian@rdla.fr>
2012-03-29 16:01:56 +0200

    Fix facebook auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,6 +2,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   FACEBOOK_LOGIN = ENV['FACEBOOK_LOGIN']
   FACEBOOK_PASSWORD = ENV['FACEBOOK_PASSWORD']
   provider :developer unless Rails.env.production?
-  provider :facebook, FACEBOOK_LOGIN, FACEBOOK_PASSWORD,
+  provider :facebook,FACEBOOK_PASSWORD , FACEBOOK_LOGIN,
            :scope => 'email', :display => 'page'
 end
