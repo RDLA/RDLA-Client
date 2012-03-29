8cca890ea760df04ddc9106425b51d9526d831a3
Damien Brian <damien.brian@rdla.fr>
2012-03-29 15:14:51 +0200

    Generate sprite sheet for field

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.2'
 
 gem 'heroku'
-
+gem "rmagick"
 #Auth
 gem 'omniauth-facebook'
 
--- app/models/field.rb
+++ app/models/field.rb
@@ -1,3 +1,17 @@
+require 'RMagick'
 class Field < ActiveResource::Base
   self.site = DB_URL
+  
+  # Internal: Get all field and create a picture containing all the
+  # file stored in database, ordered by id.
+  #
+  # Returns true if the generation success, false otherwise.
+  def self.generate_sprites
+    list = Magick::ImageList.new
+    self.all.each do |field|
+      list.read "http://assets.rdla.fr/#{field.filename}"
+    end
+    image = list.append(false)
+    image.write "#{Rails.root}/public/field.png"
+  end
 end
