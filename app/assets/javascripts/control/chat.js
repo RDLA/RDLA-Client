acb9b050a4a10137e5746192c1273a7d6f750e5a
Damien Brian <damien.brian@rdla.fr>
2012-04-04 21:10:35 +0200

    Add Chat console control

--- app/assets/javascripts/control/chat.js
+++ app/assets/javascripts/control/chat.js
@@ -1,6 +1,6 @@
 /*
  * This file contains the Chat Object. The chat object handle all informations
- * related to the Chat console. FOr example, function for sending a message, or
+ * related to the Chat console. For example, function for sending a message, or
  * function for displaying information in the Chat console.
  */
 var Chat = {
@@ -9,6 +9,77 @@ var Chat = {
   
   // Fonctions declaration
   
+  // Public: Bind all function to corresponding events
+  // 
+  // Returns nothing
+  bind_events: function()
+    {
+    //TODO Check the best event: keypress or keydown?
+    // Observing Keypress while typing in Chat Console
+    $("#command").on("keypress", Chat.observe);   
+    // Close Chat when clicking on the Icon
+    $("#close_chat").on("click", Chat.close);
+    // Open Chat when clicking on the Icon
+    $("#open_console").on("click", Chat.open);
+    },  
+  
+  
+  
+  // Public: Open the console in order to allow user to see Chat info and
+  // sending message
+  //
+  // Returns nothing
+  open: function()
+  {
+    //TODO: Replace Jquery call with Vanilla Javascript
+    if(!Chat.opened)
+    {
+			Chat.opened = true;
+			$("#console").show('fast'); 
+			$("#open_console").hide(); // We hide the button to open console.
+			$("#command").focus(); // We focus to the command input.
+    }
+  },
+
+
+
+  // Public: Close the console to give more space to the map.
+  //
+  // Returns nothing   
+  close: function()
+  {
+    //TODO: Replace Jquery call with Vanilla Javascript
+    if(Chat.opened)
+      {
+      Chat.opened = false;
+      $("#console").hide('fast'); // We hide the div console
+      $("#open_console").show(); // We show the button to reopen the console
+      }
+  },
+
+
+
+  // Public: Observe the chat console for sending the message if the user
+  // type "Enter"
+  // 
+  // evt - The jQuery.Event Triggered when user hit a key.
+  // 
+  // Returns nothing    
+  observe: function(evt)
+    {
+    var command = $.trim($("#command").val()); // We get the message being typed
+    if(evt.which == keys.enter && command != "" )
+      {
+      alert("Enter");
+      Chat.log(command); // For debug: TO DELETE After.
+      if(config.server_online)
+        Connection.send(command); // Send the message to gameserver
+      $("#command").val(""); // The message is sent, we can empty the input
+      }
+    },
+
+
+
   // Public: Display a message in the chat console. 
   //
   // msg - THe message to display in the chat console.
---
+++ app/assets/javascripts/control/start.js
@@ -0,0 +1,7 @@
+/* 
+ * This file is equivalent of main function. It start when the page
+ * is loaded
+ */
+$(function(){
+  Chat.bind_events(); // Connect events to Chat's functions
+});
